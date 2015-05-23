3a270e4583fbd7aa54ec551fbd85ae70ae2a28c5
andrew.forward <andrew.forward@cenx.com>
2015-05-23 15:37:33 -0400

    Add mix task for converting to ecto migrations

---
+++ lib/mix/a2ecto.ex
@@ -0,0 +1,26 @@
+defmodule Mix.Tasks.Ar2ecto do
+
+  use Mix.Task
+  @shortdoc "Tranform AR migrations to Ecto migrations"
+
+  @moduledoc """
+  This task will transform your active record migrations (.rb)
+  Into Ecto migrations (.exs).
+
+  For example,
+
+  ```
+  mix a2ecto MyApp ./path/to/active_record/migrations ./path/to/myapp/priv/repo/migrations
+  ```
+
+  Please report any bugs to aforward@gmail.com
+  """
+  def run(inputs) do
+    [app_name, active_record_path, ecto_path] = inputs
+    IO.puts "Migrating #{app_name} from ActiveRecord to Ecto..."
+    IO.puts "  -- Looking for migrations in #{active_record_path}"
+    IO.puts "  -- Migrating to #{ecto_path}"
+    Ar2ecto.Parser.parse(app_name, active_record_path, ecto_path)
+    IO.puts "DONE, Migrating from ActiveRecord to Ecto."
+  end
+end
