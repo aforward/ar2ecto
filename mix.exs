a62ad733d513ab1cc0e329e12e077bf688152a94
andrew.forward <andrew.forward@cenx.com>
2015-05-23 15:45:45 -0400

    Update project documentation

--- mix.exs
+++ mix.exs
@@ -1,32 +1,45 @@
 defmodule Ar2ecto.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :ar2ecto,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+
+     # Hex
+     description: description,
+     package: package,
+
+     # Docs
+     name: "Ar2ecto",
+     docs: [source_ref: "v#{@version}",
+            source_url: "https://github.com/aforward/ar2ecto"]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
+
+  defp description do
+    """
+    Ar2ecto is a set of mix tasks to help you migrate from ActiveRecord to Ecto.
+    """
+  end
+
+  defp package do
+    [contributors: ["Andrew Forward"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/aforward/ar2ecto"},
+     files: ~w(mix.exs README.md lib)]
+  end
+
 end
